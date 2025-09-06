a0d999d0546ed4030dc99d0790f786ad9b3b7d39
Kevin Ramirez <kevinrave1804@gmail.com>
2025-09-05 22:43:16 -0500

    [ADD]preview

---
+++ src/pages/Preview.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Preview() {
+    return (
+        <div>
+            <h1>Vista Previa</h1>
+            <p>Aquí puedes ver una vista previa de tu proyecto.</p>
+        </div>
+    )
+}
+
+export default Preview
